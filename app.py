cae6ea7937890e39c7a94921b44f440b05173213
foreandr <foreandr@gmail.com>
2022-06-21 17:56:22 -0400

    full sessions complete

--- app.py
+++ app.py
@@ -13,10 +13,10 @@ STATIC_DIR = os.path.abspath('./static')
 app = Flask(__name__, template_folder=TEMPLATE_DIR, static_folder=STATIC_DIR)
 app.secret_key = 'demokey'
 
-
 # user = sql_functions.check_users() #list of user in DB
 
 app.config["CSV UPLOADS"] = "C:/Users/forea/PycharmProjects/GraphTheory/#UserData"
+
 
 @app.route('/', methods=['GET'])  # homepage
 def home():
@@ -49,11 +49,44 @@ def register():
 def upload_file():
     if request.method == "POST":
         if request.files:
-            csv_file = request.files['csv'] # because name in HTML FORM is csv
+            csv_file = request.files['csv']  # because name in HTML FORM is csv
             csv_file.save(os.path.join(app.config["CSV UPLOADS"], csv_file.filename))
             print("Saved and completed")
     return render_template('upload.html', message="upload.html page")
 
+
+@app.route('/login', methods=['GET', 'POST'])  # homepage
+def login():
+    print('EXECUTING REGISTER FUNCTION')
+    if request.method == 'GET':
+        login_message = 'Please LOGIN'
+        if "email" in session:
+            return redirect(url_for("email"))
+        return render_template('login.html', message=login_message)
+    if request.method == "POST":
+        email = request.form["email"]
+        # password = request.form["password"]
+        session["email"] = email
+
+        print(session)
+        print(email)
+        # exit()
+        return redirect(url_for("email"))
+
+
+@app.route('/email', methods=['GET'])  # user_session_home
+def email():
+    if "email" in session:
+        email = session["email"]  # getting user info from session variable
+        return render_template('home.html', message=email)
+    else:  # if not in session, redirect to login
+        return redirect(url_for("login"))
+
+
+@app.route('/logout', methods=['GET'])
+def logout():
+    session.pop("email", None)  # remove data from session
+    return redirect(url_for("login"))
 
 
 if __name__ == '__main__':
