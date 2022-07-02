3c48f5ed5880d0287a97859b9b073b636c446549
foreandr <foreandr@gmail.com>
2022-07-02 09:51:21 -0400

    get friends fucntion completed

--- SQL/PROCEDURES.sql
+++ SQL/PROCEDURES.sql
@@ -1,25 +1,23 @@
+SELECT name AS procedure_name   
+    ,SCHEMA_NAME(schema_id) AS schema_name  
+    ,type_desc  
+    ,create_date  
+    ,modify_date  
+FROM sys.procedures;  
+
+DROP PROCEDURE SELECT_COMMENT_ID;  
+GO 
 GO
-ALTER PROCEDURE dbo.SELECT_USER_ID
-@ID INT
+ALTER PROCEDURE dbo.GET_FRIENDS
+@user_id INT
 AS
-SELECT * FROM dbo.USERS
-WHERE User_Id = @ID
-GO
+SELECT conn.*, users.username
+FROM dbo.CONNECTIONS conn
+INNER JOIN dbo.USERS users
+ON conn.User_Id2 = users.User_id
+WHERE User_Id1 = @user_id
 
 GO
-CREATE PROCEDURE SELECT_POST_ID
-@ID INT
-AS
-SELECT * FROM dbo.POSTS
-WHERE Post_Id = @ID
-GO
 
-GO
-CREATE PROCEDURE SELECT_COMMENT_ID
-@ID INT
-AS
-SELECT * FROM dbo.COMMENTS
-WHERE Comment_Id = @ID
-GO
 
-EXECUTE SELECT_USER_ID 17 ;
+EXECUTE GET_FRIENDS 1 ;
